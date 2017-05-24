f80c7ff463eb00ce7991cac085da25837a000f94
Alan Sanchez <jfkalostro@gmail.com>
2017-05-24 10:42:23 -0500

    Script completed and tested

--- codecept.conf.js
+++ codecept.conf.js
@@ -3,8 +3,18 @@ exports.config = {
     output: "./output",
     helpers: {
         WebDriverIO: {
-            url: "http://localhost",
-            browser: "chrome"
+            url: "http://digitalonus:d1g1t4l0nu5@exampleworkspace_exampleproject_proda.52.32.177.100.xip.io",
+            browser: "chrome",
+            user: "alansanchez3",
+            key: "Uj4ztSqqNyBsmiVG6yYg",
+            timeouts: {
+                script: 60000,
+                pageLoad: 30000
+            },
+            desiredCapabilities: {
+                "browserstack.debug": true,
+                version: "58"
+            }
         }
     },
     include: {
@@ -13,7 +23,8 @@ exports.config = {
     bootstrap: false,
     mocha: {
         reporterOptions: {
-            reportDir: "./output"
+            reportDir: "./output",
+            mochaFile: "./output/results.xml"
         }
     },
     name: "DisneyDemo"
